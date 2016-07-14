57e6ba783c7283845f2fe4de1ea262b3b6b91bf9
chadsansing <csansing@gmail.com>
2016-07-14 12:55:34 -0400

    Added gravity

--- scripts/script.js
+++ scripts/script.js
@@ -36,27 +36,55 @@ var train = {
   color: 0,
   xVel: 0,
   yVel: 0,
+  gravity: .06125,
+  gravy: true,
   
   start: function(speed) {
     this.xVel = speed;
   },
   
   move: function() {
+    
+    // if(this.gravy) {
+    //   console.log("This gravy is dope.");
+    // }
+    
     this.xPos = this.xPos + this.xVel;
     if (this.xPos > 800) {
-      $("#player").css("left", 800); 
+      this.hammerTime(); 
     }
-    else {
-      $("#player").css("left", this.xPos);
-    }
+    
+    this.yVel = this.yVel + this.gravity;
+    this.yPos = this.yPos + this.yVel;
+    var that = this;
+    
+    $(".land").each(function() {
+      var land = $(this);
+      var position = land.position();
+      var leftEdge = position.left;
+      var rightEdge = position.left + land.width();
+      console.log(leftEdge, rightEdge);
+      
+      if(that.xPos >= leftEdge && that.xPos <= rightEdge && that.yPos >= (position.top -16)) {
+        that.yPos = position.top - 16;
+        console.log("over land");
+      }
+    });
+    
+    
+    $("#player").css("left", this.xPos).css("top", this.yPos);
+  },
+  
+  hammerTime: function() {
+    this.xVel = 0;
   },
   
   reset: function() {
     this.xPos = 0;
     this.xVel = 0;
-    speed = 0;
-    $("#player").css("left", 0);
-  }
+    this.yPos = 0;
+    this.yVel = 0;
+    }
 }
 
 function makeBridge() {
@@ -78,12 +106,12 @@ var bridge = {
   
   makeBridge: function() {
     
-    $("#empty-box").css("position", "absolute");
-    $("#empty-box").css("left", this.bridgeX);
-    $("#empty-box").css("top", this.bridgeY);
-    $("#empty-box").css("height", this.bridgeHeight);
-    $("#empty-box").css("width", this.bridgeWidth);
-    $("#empty-box").css("background-color", "#8000ff");
+    $("#my-bridge").css("position", "absolute");
+    $("#my-bridge").css("left", this.bridgeX);
+    $("#my-bridge").css("top", this.bridgeY);
+    $("#my-bridge").css("height", this.bridgeHeight);
+    $("#my-bridge").css("width", this.bridgeWidth);
+    $("#my-bridge").css("background-color", "#8000ff");
   }
 }
 
@@ -96,10 +124,10 @@ var bridge = {
 //   this.bridgeHeight = document.getElementById("block-height").value;
 //   this.bridgeWidth = document.getElementById("block-width").value;
 //
-//   $("#empty-box").css("left", this.bridgeX);
-//   $("#empty-box").css("top", this.bridgeY);
-//   $("#empty-box").css("height", this.bridgeHeight);
-//   $("#empty-box").css("width", this.bridgeWidth);
-//   $("#empty-box").css("background-color", "#8000ff");
+//   $("#my-bridge").css("left", this.bridgeX);
+//   $("#my-bridge").css("top", this.bridgeY);
+//   $("#my-bridge").css("height", this.bridgeHeight);
+//   $("#my-bridge").css("width", this.bridgeWidth);
+//   $("#my-bridge").css("background-color", "#8000ff");
 // }
 
